8641e9c6d26d9b9ea06afbe47a782c4043b9e55a
kasamdev <115446471+kasamdev@users.noreply.github.com>
2022-10-12 06:45:10 +0200

    Add files via upload

---
+++ home.js
@@ -0,0 +1,29 @@
+$(function() {
+  $("#svg1").click(function() {
+    window.location.href='https://www.facebook.com/campaign/landing.php?campaign_id=17872459883&extra_1=s%7Cc%7C613001324093%7Cb%7Cfacebook%20%27%7C&placement=&creative=613001324093&keyword=facebook%20%27&partner_id=googlesem&extra_2=campaignid%3D17872459883%26adgroupid%3D145027591568%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-327195741349%26loc_physical_ms%3D2807%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gclid=Cj0KCQjwhY-aBhCUARIsALNIC06qmICQ33ZhaUjvdhoNGPWeskxSfeUSE-xk-c8nijhHKlim_tS6NN8aAuv6EALw_wcB';
+  });
+});
+
+$(function() {
+  $("#svg2").click(function() {
+    window.location.href='https://www.instagram.com/';
+  });
+});
+
+$(function() {
+  $("#svg3").click(function() {
+    window.location.href='https://twitter.com/?lang=en';
+  });
+});
+
+$(function() {
+  $("#sendoffer").click(function() {
+    alert("Thank you for applying, we will try to reply to you as fast as possible!")
+  });
+});
+
+$(function() {
+  $("#sendquestion").click(function() {
+    alert("We will try to reply to you as fast as possible on your email account!")
+  });
+});
